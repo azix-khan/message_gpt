7639d53569fb35fb920a18acafd8b6d9f91b9ae2
azix khan <azixkhan.55@gmail.com>
2024-07-20 16:38:39 +0500

    debugging

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:message_gpt/constants/colors.dart';
 import 'package:dash_chat_2/dash_chat_2.dart';
+import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
+import 'package:message_gpt/constants/consts.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -10,12 +12,20 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  // initilized openAI object
+  final _openAI = OpenAI.instance.build(
+    token: OPENAI_API_KEY,
+    baseOption: HttpSetup(
+      receiveTimeout: const Duration(seconds: 5),
+    ),
+    enableLog: true,
+  );
   final ChatUser _currentUser =
       ChatUser(id: '1', firstName: "azix", lastName: "khan");
   final ChatUser _gptChatUser =
       ChatUser(id: '2', firstName: "Chat", lastName: "Gpt");
 
-  List<ChatMessage> _messages = <ChatMessage>[];
+  final List<ChatMessage> _messages = <ChatMessage>[];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,16 +37,17 @@ class _ChatScreenState extends State<ChatScreen> {
         ),
       ),
       body: DashChat(
-          currentUser: _currentUser,
-          messageOptions: const MessageOptions(
-            currentUserContainerColor: goldenColor,
-            containerColor: whiteColor,
-            textColor: blackColor,
-          ),
-          onSend: (ChatMessage m) {
-            getChatResponse(m);
-          },
-          messages: _messages),
+        currentUser: _currentUser,
+        messageOptions: const MessageOptions(
+          currentUserContainerColor: goldenColor,
+          containerColor: whiteColor,
+          textColor: blackColor,
+        ),
+        onSend: (ChatMessage m) {
+          getChatResponse(m);
+        },
+        messages: _messages,
+      ),
     );
   }
 
@@ -45,5 +56,36 @@ class _ChatScreenState extends State<ChatScreen> {
     setState(() {
       _messages.insert(0, m);
     });
+    // context of previous messeages
+    List<Map<String, dynamic>> messagesHistory =
+        _messages.reversed.toList().map((m) {
+      if (m.user == _currentUser) {
+        return Messages(role: Role.user, content: m.text).toJson();
+      } else {
+        return Messages(role: Role.assistant, content: m.text).toJson();
+      }
+    }).toList();
+    final request = ChatCompleteText(
+      model: GptTurboChatModel(),
+      messages: messagesHistory,
+      maxToken: 200,
+    );
+
+    // Handling API Responses
+    final respone = await _openAI.onChatCompletion(request: request);
+    for (var element in respone!.choices) {
+      if (element.message != null) {
+        setState(() {
+          _messages.insert(
+            0,
+            ChatMessage(
+              user: _gptChatUser,
+              createdAt: DateTime.now(),
+              text: element.message!.content,
+            ),
+          );
+        });
+      }
+    }
   }
 }
